d6906d4ccee838070f96af0ac8e8ea8689a6251a
Tony Bui <tony.bui.jp@gmail.com>
2022-04-20 18:05:59 +0700

    [70] Climbing stairs

---
+++ 07-dynamic_programming/70_climbing_stair.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# @param {Integer} n
+# @return {Integer}
+def climb_stairs(n)
+  
+end
+
+
+# n = 2
+# 1 + 1
+# 2
+# => 2
+
+# n = 3
+# 1 + 1 + 1
+# 2 + 1
+# 1 + 2
