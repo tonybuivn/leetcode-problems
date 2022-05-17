fec40963a13faf704acf35b916933b1480e494cb
Tony Bui <tony.bui.jp@gmail.com>
2022-05-17 17:07:15 +0700

    202 - Happy Number

---
+++ 02-math/202_happy_number.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# @param {Integer} n
+# @return {Boolean}
+def is_happy(n)
+  slow = n
+  fast = n
+  loop do
+    slow = digit_square_sum(slow)
+    fast = digit_square_sum(fast)
+    fast = digit_square_sum(fast)
+    break if slow == fast
+  end
+
+  return true if slow == 1
+
+  false
+end
+
+def digit_square_sum(n)
+  sum = 0
+  while n != 0
+    temp = n % 10
+    sum += temp * temp
+    n /= 10
+  end
+  sum
+end
+
+num = 19
+puts is_happy(num)
