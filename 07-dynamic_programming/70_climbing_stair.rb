3e2a8936c4bed681b7580d509200dd2d2086a44e
Tony Bui <tony.bui.jp@gmail.com>
2022-06-15 14:20:40 +0700

    [70] - Climbing Stairs

--- 07-dynamic_programming/70_climbing_stair.rb
+++ 07-dynamic_programming/70_climbing_stair.rb
@@ -2,17 +2,27 @@
 
 # @param {Integer} n
 # @return {Integer}
-def climb_stairs(n)
-  
+def climb_stairs(num)
+  climb_stairs_helper(num, [])
 end
 
+def climb_stairs_helper(num, cache)
+  return num if num <= 3
 
-# n = 2
-# 1 + 1
-# 2
-# => 2
+  cache[num] = climb_stairs_helper(num - 1, cache) + climb_stairs_helper(num - 2, cache) unless cache[num]
+  cache[num]
+end
 
-# n = 3
-# 1 + 1 + 1
-# 2 + 1
-# 1 + 2
+# @param {Integer} num
+# @return {Integer}
+# iterative way
+def climb_stairs_iterative(num)
+  return num if num <= 3
+
+  dp = [0, 1, 2, 3]
+  (4..num).each { |n| dp[n] = dp[n - 1] + dp[n - 2] }
+
+  dp[num]
+end
+
+puts climb_stairs_iterative(5)
